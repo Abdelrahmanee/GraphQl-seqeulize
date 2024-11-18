99f6bce87d2948a33e5882aa44c93a3e9df493de
Abdelrahmanee <123949503+Abdelrahmanee@users.noreply.github.com>
2024-11-19 00:52:40 +0200

    complete user CRUD

--- src/graphql/resolvers/user.resolver.js
+++ src/graphql/resolvers/user.resolver.js
@@ -2,10 +2,50 @@ import User from "../../models/user.model.js";
 
 export const userResolvers = {
   Query: {
-    users: async () => User.findAll(),
-    user: async (_, { id }) => User.findByPk(id),
+    users: async () => {
+      try {
+        return await User.findAll();
+      } catch (error) {
+        throw new Error("Error fetching users: " + error.message);
+      }
+    },
+    user: async (parent, { id }) => {
+      try {
+        const user = await User.findByPk(id);
+        if (!user) throw new Error("User not found");
+        return user;
+      } catch (error) {
+        throw new Error("Error fetching user: " + error.message);
+      }
+    },
   },
-  User: {
-    comments: (user) => user.getComments(),
+  Mutation: {
+    createUser: async (parent, { input }) => {
+      try {
+        return await User.create(input);
+      } catch (error) {
+        throw new Error("Error creating user: " + error.message);
+      }
+    },
+    updateUser: async (parent, { id, input }) => {
+      try {
+        const user = await User.findByPk(id);
+        if (!user) throw new Error("User not found");
+        await user.update(input);
+        return user;
+      } catch (error) {
+        throw new Error("Error updating user: " + error.message);
+      }
+    },
+    deleteUser: async (parent, { id }) => {
+      try {
+        const user = await User.findByPk(id);
+        if (!user) throw new Error("User not found");
+        await user.destroy();
+        return true;
+      } catch (error) {
+        throw new Error("Error deleting user: " + error.message);
+      }
+    },
   },
 };
--- src/graphql/schemas/user.schema.js
+++ src/graphql/schemas/user.schema.js
@@ -1,14 +1,29 @@
 
-export const userTypeDef = /* GraphQL */ `
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    comments: [Comment]
-  }
+export const typeDefs = /* GraphQL */ `
+type Query {
+  users: [User!]!
+  user(id: ID!): User
+}
 
-  extend type Query {
-    users: [User]
-    user(id: ID!): User
-  }
+type Mutation {
+  createUser(input: NewUserInput!): User!
+  updateUser(id: ID!, input: UpdateUserInput!): User!
+  deleteUser(id: ID!): Boolean!
+}
+
+type User {
+  id: ID!
+  name: String!
+  email: String!
+}
+
+input NewUserInput {
+  name: String!
+  email: String!
+}
+
+input UpdateUserInput {
+  name: String
+  email: String
+}
 `;
